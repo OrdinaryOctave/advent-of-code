23578820a8d2fe47114ed39e51b1157e5ca1434f
SimeonIsHere <simeon@simeonjack.com>
2021-12-02 11:03:11 +0000

    this doesn't work yet but it's the first version of chall2 that runs

--- Day1/chall2.py
+++ Day1/chall2.py
@@ -1,1 +1,36 @@
-#challenge 2
+def avg(array):
+    if len(array) == 0:
+        return 0
+    sum=0
+    for num in array:
+        sum += num
+    average = sum/len(array)
+    return average
+
+depthFile = open("Day1/input.txt","r")
+
+#todo: make the variables have sensible names
+depthArray1=[]
+depthArray2=[]
+depthArray3=[]
+depthArray4=[]
+depthIncreases=0
+
+for line in depthFile:
+    currentDepth = int(line)
+
+    #there's definitely a better way to do this but this code works i guess?
+    depthArray4 = depthArray3
+    depthArray3 = depthArray2
+    depthArray3.append(currentDepth)
+    depthArray2 = depthArray1
+    depthArray2.append(currentDepth)
+    depthArray1 = [currentDepth]
+
+    if avg(depthArray4) < avg(depthArray3):
+        depthIncreases += 1
+        print (depthIncreases)
+        #print (depthArray4)
+        #print (depthArray3)
+
+print (depthIncreases)
