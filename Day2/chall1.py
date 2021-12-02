2d5c211bce1069d864f5a2b15a2e84433f2bcbb1
SimeonIsHere <simeon@simeonjack.com>
2021-12-02 11:32:57 +0000

    day 2

---
+++ Day2/chall1.py
@@ -0,0 +1,17 @@
+instructionsFile = open("Day2/input.txt","r")
+instructions = instructionsFile.readlines()
+
+hPos = 0
+depth = 0
+for instruction in instructions:
+    splitInstruction = instruction.split()
+    if splitInstruction[0] == "forward":
+        hPos += int(splitInstruction[1])
+    elif splitInstruction[0] == "down":
+        depth += int(splitInstruction[1])
+    elif splitInstruction[0] == "up":
+        depth -= int(splitInstruction[1])
+
+print (hPos)
+print (depth)
+print (hPos*depth)
