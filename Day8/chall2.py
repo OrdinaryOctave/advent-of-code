e477b3e3b649c2c063509586a884614053e6c7cd
SimeonIsHere <simeonishere@protonmail.com>
2021-12-08 05:26:37 +0000

    unfinished solution

---
+++ Day8/chall2.py
@@ -0,0 +1,38 @@
+file = open("Day8/input.txt","r")
+values = file.readlines()
+outputs = []
+for value in values:
+    outputs.append((value.split("|")[1]).split())
+
+outputValues = []
+for output in outputs:
+    valueString = ""
+    for digit in output:
+        if len(digit) == 2:
+            valueString += "1"
+        elif len(digit) == 3:
+            valueString += "7"
+        elif len(digit) == 4:
+            valueString += "4"
+        elif len(digit) == 7:
+            valueString += "8"
+        elif len(digit) == 6:
+            if 'f' not in digit:
+                valueString += "0"
+            elif 'a' not in digit:
+                valueString += "6"
+            else:
+                valueString += "9"
+        elif len(digit) == 5:
+            if 'a' not in digit:
+                valueString += "5"
+            elif 'b' not in digit:
+                valueString += "2"
+            else:
+                valueString += "3"
+    outputValues.append(int(valueString))
+
+total = 0
+for value in outputValues:
+    total += value
+print(total)
