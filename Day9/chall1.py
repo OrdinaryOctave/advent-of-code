a06dcc9f3c05e81f4bd745ac54c2ffe8497a0220
SimeonIsHere <simeon@simeonjack.com>
2021-12-09 09:28:57 +0000

    first attempt at day 9 not yet functioning

---
+++ Day9/chall1.py
@@ -0,0 +1,32 @@
+file = open ("Day9/input.txt","r")
+heightMapStrings = file.readlines()
+
+heightMap=[]
+for line in heightMapStrings:
+    lineArray=[]
+    for i in range(len(line)):
+        try:
+            lineArray.append(int(line[i]))
+        except:
+            print("hi")
+    heightMap.append(lineArray)
+
+totalRisk = 0
+for i in range(len(heightMap)):
+    for j in range(len(heightMap[i])-1):
+        lowPoint = True
+        if j>0:
+            if heightMap[i][j]>=heightMap[i][j-1]:
+                lowPoint = False
+        if j+1<len(heightMap):
+            if heightMap[i][j]>=heightMap[i][j+1]:
+                lowPoint = False
+        if i+1<len(heightMap[i]):
+            if heightMap[i][j]>=heightMap[i+1][j]:
+                lowPoint = False
+        if i>0:
+            if heightMap[i][j]>=heightMap[i-1][j]:
+                lowPoint = False
+        if lowPoint:
+            totalRisk += heightMap[i][j]+1
+print (totalRisk)
