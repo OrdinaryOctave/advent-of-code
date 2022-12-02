8244ca55e1dc3fb3bd34087fc91d0a9980ac5163
OrdinaryOctave <12157391+OrdinaryOctave@users.noreply.github.com>
2022-12-02 06:49:00 +0000

    Update setup script Check if file exists before making request, avoid unnecessary requests

--- dailySetup.py
+++ dailySetup.py
@@ -6,14 +6,22 @@ cookie_file = "sessionCookie.txt"
 now = datetime.datetime.now()
 
 with open(cookie_file) as f:
-    session_cookie = f.read();
+    session_cookie = f.read()
 
-r = requests.get(f"https://adventofcode.com/{now.year}/day/{now.day}/input", cookies={"session": session_cookie})
+if (now.hour<5):
+    now = now - datetime.timedelta(days = 1)
+    print("Not 5am yet, loading previous day's challenge")
 
 inputFilePath = f"inputs/day{now.day}"
 
-with open(inputFilePath, "w") as f:
-    f.write(r.text)
+try:
+    with open(inputFilePath, "x") as f:
+        print("Making request for input:")
+        r = requests.get(f"https://adventofcode.com/{now.year}/day/{now.day}/input", cookies={"session": session_cookie})
+        print("Saving input to file:")
+        f.write(r.text)
+except:
+    print("Input file already exists for today, skipping input load")
 
 solutionFilePath = f"solutions/day{now.day}.py"
 
@@ -24,9 +32,11 @@ boilerplateSolution = f"""with open('inputs/day{now.day}') as f:
 
 try:
     with open(solutionFilePath, "x") as f:
+        print("Creating boilerplate solution:")
         f.write(boilerplateSolution)
 except:
-    print("Solution file already exists for today")
+    print("Solution file already exists for today, skipping solution file create")
 
-print(f"Starting today's challenge at {now.time().strftime('%H:%M:%S')}, good luck!")
+now = datetime.datetime.now()
+print(f"Loading complete\nStarting today's challenge at {now.time().strftime('%H:%M:%S')}\nGood luck!")
 
