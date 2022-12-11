dfbc492b20b1ce2032d6f6a75bdccc1241875137
OrdinaryOctave <12157391+OrdinaryOctave@users.noreply.github.com>
2022-12-11 21:19:36 +0000

    update day 11 import from input file and do both parts within the code defined

--- solutions/day11.py
+++ solutions/day11.py
@@ -1,6 +1,5 @@
 #input loaded and ready to go at 15:50:30
-# Hard coding the monkeys because it's easier - this won't work with other inputs
-# This code is only part 2 - I just modified part 1 code to produce the resuls
+from copy import deepcopy
 
 class Monkey:
     def __init__(self, items, op, testOperand, ifTrue, ifFalse):
@@ -11,10 +10,10 @@ class Monkey:
         self.ifFalse = ifFalse
         self.itemCounter = 0
     
-    def processItems(self):
+    def processItems(self, worryDivider):
         for item in self.items:
             self.itemCounter += 1
-            item = eval(self.op)%lcm
+            item = (eval(self.op)%lcm) // worryDivider
             if item%self.testOperand == 0:
                 monkeys[self.ifTrue].addItem(item)
             else:
@@ -24,30 +23,51 @@ class Monkey:
     def addItem(self, item):
         self.items.append(item)
         
+def getMonkeyBusiness(monkeys):
+    maxMonkey = 0
+    secondMonkey = 0
 
-monkeys = [Monkey([71, 56, 50, 73], "item*11", 13, 1, 7)]
-monkeys.append(Monkey([70, 89, 82], "item+1", 7, 3, 6))
-monkeys.append(Monkey([52, 95], "item*item", 3, 5, 4))
-monkeys.append(Monkey([94, 64, 69, 87, 70], "item+2", 19, 2, 6))
-monkeys.append(Monkey([98, 72, 98, 53, 97, 51], "item + 6", 5, 0, 5))
-monkeys.append(Monkey([79], "item+7", 2, 7, 0))
-monkeys.append(Monkey([77, 55, 63, 93, 66, 90, 88, 71], "item*7", 11, 2, 4))
-monkeys.append(Monkey([54, 97, 87, 70, 59, 82, 59], "item+8", 17, 1, 3))
-lcm = 9699690 # lowest common multiple of all testOperands
+    for monkey in monkeys:
+        if monkey.itemCounter>maxMonkey:
+            secondMonkey = maxMonkey
+            maxMonkey = monkey.itemCounter
+        else:
+            secondMonkey = max(monkey.itemCounter, secondMonkey)
+    
+    return maxMonkey*secondMonkey
+
+def importMonkeys(monkeyString):
+    monkeys = []
+    monkeyInputs = monkeyString.split("\n\n")
+    for monkey in monkeyInputs:
+        monkeyLines = monkey.split('\n')
+        items = monkeyLines[1].replace("  Starting items: ", "").split(", ")
+        items = [int(item) for item in items]
+        op = monkeyLines[2].replace("  Operation: new = ", "")
+        op = op.replace("old", "item")
+        testOperand = int(monkeyLines[3].replace("  Test: divisible by ", ""))
+        ifTrue = int(monkeyLines[4].replace("    If true: throw to monkey ", ""))
+        ifFalse = int(monkeyLines[5].replace("    If false: throw to monkey ", ""))
+        
+        monkeys.append(Monkey(items, op, testOperand, ifTrue, ifFalse))
+    return monkeys
+    
+with open('inputs/day11') as f:
+    input = f.read().rstrip()
+
+monkeys = importMonkeys(input)
+lcm = 1
+for monkey in monkeys:
+    lcm = lcm * monkey.testOperand
+
+for i in range(20):
+    for monkey in monkeys:
+        monkey.processItems(3)
+print(getMonkeyBusiness(monkeys))
+
+monkeys = importMonkeys(input)
 
 for i in range(10000):
     for monkey in monkeys:
-        monkey.processItems()
-    print(i)
-
-maxMonkey = 0
-secondMonkey = 0
-
-for monkey in monkeys:
-    if monkey.itemCounter>maxMonkey:
-        secondMonkey = maxMonkey
-        maxMonkey = monkey.itemCounter
-    else:
-        secondMonkey = max(monkey.itemCounter, secondMonkey)
-
-print(maxMonkey*secondMonkey)
+        monkey.processItems(1)
+print(getMonkeyBusiness(monkeys))
