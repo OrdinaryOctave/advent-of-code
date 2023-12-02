604892cad807234bff0dd58b0368dc6f1a9d50c0
OrdinaryOctave <12157391+OrdinaryOctave@users.noreply.github.com>
2023-12-02 07:42:26 +0000

    clean up day 2

--- 2023/solutions/day2.py
+++ 2023/solutions/day2.py
@@ -5,38 +5,21 @@ with open('2023/inputs/day2') as f:
 
 games = input.split('\n')
 
-# PART 1
-maxValues = {
-    "r" : 12,
-    "g" : 13,
-    "b" : 14
+maxCubes = {
+    "red"   : 12,
+    "green" : 13,
+    "blue"  : 14
 }
-
 validGamesSum = 0
+totalPower = 0
 
 for i, game in enumerate(games):
     gameFailed = False
-    rounds = game.split(": ")[1].split("; ")
-    for round in rounds:
-        marbles = round.split(", ")
-        for marble in marbles:
-            count, colour = marble.split(" ")
-            if (int(count) > maxValues[colour[:1]]):
-                gameFailed = True
-    if not gameFailed:
-        validGamesSum += i+1
-
-print(validGamesSum)
-
-#PART 2
-totalPower = 0
-
-for game in games:
     gamePower = 1
     minCubes = {
-    "r" : 0,
-    "g" : 0,
-    "b" : 0
+    "red"   : 0,
+    "green" : 0,
+    "blue"  : 0
     }
     
     rounds = game.split(": ")[1].split("; ")
@@ -44,11 +27,15 @@ for game in games:
         marbles = round.split(", ")
         for marble in marbles:
             count, colour = marble.split(" ")
-            colour = colour[:1]
+            if (int(count) > maxCubes[colour]):
+                gameFailed = True
             minCubes[colour] = max(minCubes[colour], int(count))
-            
+    
+    if not gameFailed:
+        validGamesSum += i+1
     for value in minCubes.values():
         gamePower *= value
     totalPower += gamePower
-    
+
+print(validGamesSum)
 print(totalPower)    
