50af4cbd5f639bd8c75555a1352f9478f2e04d6b
OrdinaryOctave <12157391+OrdinaryOctave@users.noreply.github.com>
2023-12-03 12:45:18 +0000

    improve day 3

--- 2023/solutions/day3.py
+++ 2023/solutions/day3.py
@@ -1,54 +1,42 @@
 # input loaded at 05:00:03
 
-def checkPartNumber(y, minX, maxX):
-    partNum = int(schematic[y][minX:maxX+1])
-    validPart = False
-    if minX == 0:
-        minX += 1
-    if maxX == len(schematic[0])-1:
-        maxX -=1
-    coords = [(x,y) for x in range(minX-1, maxX+2)]
-    if y != 0:
-        coords += [(x,y-1) for x in range(minX-1, maxX+2)]
-    if y != len(schematic)-1:
-        coords += [(x,y+1) for x in range(minX-1, maxX+2)]
-        
-    for x, z in coords:
-        char = schematic[z][x]
-        if not char.isdigit() and char != '.':
-            validPart = True
-            if char == '*':              
-                if (x,z) in gears:
-                    gears[(x,z)].append(partNum)
-                else:
-                    gears[(x,z)] = [partNum]
-                
-    return validPart
-
 with open('2023/inputs/day3') as f:
     input = f.read()
 
-schematic = [line for line in input.split('\n')]
+schematic = input.split('\n')
+rowLength = len(schematic[0])
 parts = []
+
+for x, row in enumerate(schematic):
+    enumeratedRow = enumerate(row)
+    for y, char in enumeratedRow:
+        if char.isdigit():
+            parts.append([(x,y), 1])
+            while next(enumeratedRow, (0, '.'))[1].isdigit():
+                parts[-1][1] += 1
+            
+validParts = []
 gears = {}
 
-for i, row in enumerate(schematic):
-    minJ = -1
-    for j, char in enumerate(row):
-        if char.isdigit():
-            maxJ = j
-            if minJ == -1:
-                minJ = j
-            continue
-        if minJ == -1:
-            continue
-        if checkPartNumber(i, minJ, maxJ):
-            parts.append(int(row[minJ:j]))
-        minJ = -1
-    if minJ != -1 and checkPartNumber(i, minJ, len(row)-1):
-        parts.append(int(row[minJ:len(row)]))
-            
-print(sum(parts))
+for (x, y), length in parts:
+    partNum = int(schematic[x][y:y+length])
+    neighbourCoords = []
+    for row in range(max(x-1, 0), min(len(schematic), x+2)):
+        neighbourCoords += [(row, col) for col in range(max(y-1, 0), min(y+length+1, rowLength))]
+    validPart = False
+    for row, col in neighbourCoords:
+        char = schematic[row][col]
+        if not char.isdigit() and char != '.':
+            validPart = True
+            if char == '*':
+                if (row, col) in gears:
+                    gears[(row, col)].append(partNum)
+                else:
+                    gears[(row, col)] = [partNum]
+    if validPart:
+        validParts.append(partNum)
+    
+print(sum(validParts))
 
 gearRatioSum = 0
 for gear in gears.values():
